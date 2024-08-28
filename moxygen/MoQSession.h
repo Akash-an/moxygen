eed4a3887ebaeee94d886e228b072dd61d5c1171
Akash <akashn@akamai.com>
2024-08-28 13:47:12 +0000

    increased cache size

--- moxygen/MoQSession.h
+++ moxygen/MoQSession.h
@@ -263,7 +263,7 @@ class MoQSession : public MoQCodec::Callback {
     
     //using this instead of objects_
     folly::EvictingCacheMap<std::pair<uint64_t, uint64_t>, std::shared_ptr<ObjectSource>> 
-        object_cache_ = folly::EvictingCacheMap<std::pair<uint64_t, uint64_t>, std::shared_ptr<ObjectSource>>(1000);
+        object_cache_ = folly::EvictingCacheMap<std::pair<uint64_t, uint64_t>, std::shared_ptr<ObjectSource>>(5000);
     
     folly::coro::UnboundedQueue<std::shared_ptr<ObjectSource>, true, true>
         newObjects_;
