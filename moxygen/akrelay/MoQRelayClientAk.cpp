77723909a0305fd3a88661d7ca6dc9701cc4448a
Akash <akashn@akamai.com>
2024-09-02 09:32:14 +0000

    temp commit to switch back

--- moxygen/akrelay/MoQRelayClientAk.cpp
+++ moxygen/akrelay/MoQRelayClientAk.cpp
@@ -30,7 +30,14 @@ namespace moxygen {
     }
 
     void MoQRelayClientAk::addDownstreamSession(std::shared_ptr<MoQSession> upstreamsession, std::shared_ptr<MoQSession> downstreamSession){
-        controlMessageHandler_.data_.downstreamSessions_[upstreamsession].insert(std::move(downstreamSession));
+        // controlMessageHandler_.data_.downstreamSessions_[upstreamsession].insert(std::move(downstreamSession));
+        auto it = controlMessageHandler_.data_.downstreamSessions_.find(upstreamsession);
+        if(it != controlMessageHandler_.data_.downstreamSessions_.end()){
+            it->second.insert(std::move(downstreamSession));
+        }
+        else{
+            it->second = {std::move(downstreamSession)};
+        }
         XLOG(INFO) << __func__ << " sessions size=" << controlMessageHandler_.data_.downstreamSessions_.size();
     }
 
